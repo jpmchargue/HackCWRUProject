2ef86d418e349e8a71ccfe06ba3f3566279bc2fc
jpmchargue <jamespm4@illinois.edu>
2020-02-07 23:52:50 -0500

    Very basic GUI mockup.

--- main.php
+++ main.php
@@ -2,11 +2,43 @@
 <head>
 <style>
 
+.chart {
+	border: 1px solid #000;
+	height: 150px;
+}
+.blocks {
+	height: 80px;
+}
+.codespace {
+	border: 1px solid #000;
+	height: 200px;
+	padding: 10px;
+	background: #ccc;
+}
+.console {
+	border: 1px solid #000;
+	height: 80px;
+	padding: 10px;
+	background: #222;
+	color: #fff;
+}
+
 </style>
 </head>
 <body>
 
-<?php echo "Hello world! :)"; ?>
+<div class="chart" id="chart">
+chart
+</div>
+<div class="blocks" id="blocks">
+blocks
+</div>
+<div class="codespace" id="codespace">
+code
+</div>
+<div class="console" id="console">
+console
+</div>
 
 </body>
 </html>
